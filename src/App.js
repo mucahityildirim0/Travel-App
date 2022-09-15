6068c11708cd04ebfeb889cc59cf7cba1070a6c1
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-09-15 15:50:48 +0300

    creating home pages and hero component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navbar />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import "../App.css";
+import Button from "./Button";
+import "./HeroSection.css";
+import video2 from "../videos/video-2.mp4";
 
 const HeroSection = () => {
   return (
-    <div>
-      <h1>sel</h1>
+    <div className="hero-container">
+      <video src={video2} autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          WATCH TRAILER <i className="far fa-play-circle"></i>
+        </Button>
+      </div>
     </div>
   );
 };
